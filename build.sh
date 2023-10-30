3a4ef96faa994d63550bfb4ff385bbd7487836da
Bastian Reuther <bastir25@icloud.com>
2023-10-30 23:34:02 +0100

    ci

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ cp enable-threads.js dist/
 echo 'done.'
 
 echo 'Building for linux ...'
-godot --headless --no-window --export-release 'Linux/X11' dist/roundtrip.x86_64
-chmod a+x dist/roundtrip.x86_64
+godot --headless --no-window --export-release 'Linux/X11' dist/BinaryBlasters.x86_64
+chmod a+x dist/BinaryBlasters.x86_64
 echo 'done.'
 
 echo 'Building for Windows ...'
-godot --headless --no-window --export-release 'Windows Desktop' dist/roundtrip.exe
+godot --headless --no-window --export-release 'Windows Desktop' dist/BinaryBlasters.exe
 echo 'done.'
