ebe6f5ff04de6b2b6ba6ed8d9091786847e78289
Bastian Reuther <bastian.reuther@allod.com>
2023-11-01 20:09:40 +0100

    add other enemy and change ingame ui

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -3,8 +3,9 @@ extends Node2D
 var running = true
 var spawn_positions = null
 var Enemy = preload("res://scene/enemy.tscn")
+var Enemy2 = preload("res://scene/enemy2.tscn")
 @onready var spawnTimer = $Spawntimer
-
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,7 +23,12 @@ func _process(delta):
 func spawn_enemy():
 	if running:
 		var index = randi() % spawn_positions.size()
-		var enemy = Enemy.instantiate()
+		var enemy
+		var random = rng.randf_range(0,100)
+		if int(random) % 2 == 1:
+			enemy = Enemy.instantiate()
+		else: 
+			enemy = Enemy2.instantiate()
 		enemy.global_position = spawn_positions[index].global_position
 		add_child(enemy)
 	
